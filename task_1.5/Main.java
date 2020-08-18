b36ef8f4827f9075dbd67530f28d5554d4b7bbfa
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-18 23:29:59 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,1 +1,9 @@
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float TC = scan.nextFloat();
+        float a = 9;
+        float b = 5;
+        System.out.println ((a/b)*TC+32);
+    }
