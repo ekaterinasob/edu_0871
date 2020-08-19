9c2e4ae00f17dc08996cccc7719fb453fe26304f
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-19 20:10:47 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -1,1 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int sum = 1;
+        for (int i = 1; i < 10; i++) {
+            sum*=nums[i];
+
+        }
+        System.out.println(sum);
+    }
+
+    }
 
