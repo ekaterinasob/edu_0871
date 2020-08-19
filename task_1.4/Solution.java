8017d41eaea24c2c6396a79dc0e65f5c0777f82f
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-19 14:35:57 +0300

    Update Solution.java

--- task_1.4/Solution.java
+++ task_1.4/Solution.java
@@ -1,1 +1,10 @@
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println(number * number);
+    }
+}
+
 
