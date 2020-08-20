26b0673cabfc6fb3d8c5ccfcad412280763e202d
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-20 23:02:30 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static int min(int a, int b, int c) {
-        return (a<b)?((a<c)?a:c) : ((b<c)? b:c);
+        return a<=b?a<=c?a:c : b<=c? b:c ;
 
     }
     public static void main(String[] args) {
