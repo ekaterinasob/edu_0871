ed990fc70ad9f1d30f5b7c3298af8b32013e9b61
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-21 23:41:53 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -1,4 +1,5 @@
- public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
                 for (int j = 1; j < 11; j++) {
                     System.out.print(i * j + " ");
