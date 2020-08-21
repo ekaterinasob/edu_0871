bade2aa4fef3d2238197feb29cd8080f241d33f8
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-21 14:09:42 +0300

    Update Main.java

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -1,1 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i =0;
+        for (int j = 0; j < 5; j++) {
+            i=i+j+1;
+                System.out.println(i);
+            }
+
+
+        }
+
+    }
 
