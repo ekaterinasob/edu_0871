6cba5917b05edf8a2fea9b975620413b7f5089ab
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-21 19:30:04 +0300

    Update Main.java

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -1,1 +1,12 @@
+ public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+                for (int j = 1; j < 11; j++) {
+                    System.out.print(i * j + " ");
+                }
+                System.out.println("");
 
+
+                }
+            }
+        }
+    
