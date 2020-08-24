745dd608e0e417996e361173c3b10c2938011087
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-24 22:42:32 +0300

    Update Dog.java

--- task_3.3/Dog.java
+++ task_3.3/Dog.java
@@ -1,1 +1,36 @@
+public class Dog {
+    
+    public static void main(String[] args) {
+        Dog maylo = new Dog(name"Maylo", age 3);
+
+    }
+
+}
+
+class Dog{
+    private String name ;
+    private int age ;
+
+    public Dog(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
 
