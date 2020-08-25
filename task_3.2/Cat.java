ffce4190a566808ed1f784ba71277f814ba0a54a
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-25 16:01:45 +0300

    Update Cat.java

--- task_3.2/Cat.java
+++ task_3.2/Cat.java
@@ -1,1 +1,23 @@
+public class Cat {
+
+    private String name = "безымянный кот";
+
+
+    public Cat() {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+
+    }
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+
+        cat.setName("Жужик");
+
+        System.out.println(cat.name);
+    }
+  }
+
 
