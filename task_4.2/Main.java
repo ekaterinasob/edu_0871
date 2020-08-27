7c454b58a3ffe145e893b97257a2d93bba7d5f5f
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-27 16:41:30 +0300

    Update Main.java

--- task_4.2/Main.java
+++ task_4.2/Main.java
@@ -1,1 +1,67 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
+        Dog spike = new Dog ("Spike", 20, 5, "gray");
+        Cat tom = new Cat ("Tom", 17, 9,"black");
+        
+
+    }
+
+}
+
+class Dog{
+    String name;
+    int height;
+    int tail;
+    String color;
+
+    public Dog(String name, int height, int tail, String color) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+        this.color = color;
+    }
+
+}
+
+class Cat{
+    String name;
+    int height;
+    int tail;
+    String color;
+
+    public Cat(String name, int height, int tail, String color) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+        this.color = color;
+    }
+}
+
+class Mouse {
+    String name;
+    int height;
+    int tail;
+
+    public Mouse(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
