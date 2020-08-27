ec03ff81621ba24a4a55beeba117c395369d94f1
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-27 16:04:32 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -1,26 +1,34 @@
-*//public class Main {
-    public static int min(int a, int b, int c, int d) {
-        if (min()>a<c?a:c : b<c? b:c : a<d?a:d : b<d?b:d {
-        
+public class Main {
 
-    }
+    public static int min(int a, int b, int c, int d) {
+        if (c<d) return c;
+        else return min(a, b);
+
+
+
+        }
 
 
 
     public static int min(int a, int b) {
-        return a>b?b:a;
+        if (a<b) return a;
+        else return b;
+
+
 
     }
 
     public static void main(String[] args) throws Exception {
 
         System.out.println(min(-20, -10));
+
         System.out.println(min(-40, -10, -30, 40));
+
         System.out.println(min(-20, -40, -30, 40));
+
         System.out.println(min(-20, -10, -40, 40));
+
         System.out.println(min(-20, -10, -30, -40));
 
     }
-
 }
-
