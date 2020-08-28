a409b036059081d5354815dac0686f3d4d8075e6
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-28 11:27:05 +0300

    Update Main.java

--- task_2.7/Main.java
+++ task_2.7/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
 
+    }
+    public static int sumDigitsInNumber(int number) {
+        return (number/100)+(number%100)/10+(number%100)%10;
+
+    }
+
+}
