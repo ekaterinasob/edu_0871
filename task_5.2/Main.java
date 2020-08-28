098a5ff4d14d86a18307b130437f8d15dbb9ee58
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-08-29 02:57:34 +0300

    Update Main.java

--- task_5.2/Main.java
+++ task_5.2/Main.java
@@ -1,1 +1,37 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Укажите имя файла и нажмите Enter ");
+
+        Scanner scan = new Scanner(System.in);
+        String filename = scan.nextLine();
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        System.out.println(parseLines(reader));
+
+        }
+
+    private static Object parseLines(BufferedReader reader)throws IOException {
+        ArrayList<Integer> numslist = new ArrayList<>();
+        StringBuilder list  = new StringBuilder();
+        String line = "";
+        while ((line = reader.readLine()) !=null) {
+            if (Integer.parseInt(line)%2==0) numslist.add(Integer.parseInt(line));
+
+        }
+        Collections.sort(numslist);
+        for (int i =0; i< numslist.size(); i++) {
+            list.append(numslist.get(i));
+            list.append(System.lineSeparator());
+        }
+        return list.toString();
+
+    }
+
+}
