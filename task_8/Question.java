512acbbf3d4e16bccf9f639a6247c5a5b5dfdb9f
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-09-17 12:47:42 +0300

    Update Question.java

--- task_8/Question.java
+++ task_8/Question.java
@@ -1,1 +1,27 @@
+package com.example.myapplication;
 
+public class Question {
+    private int questionTextId;
+    private boolean answerTrue;
+
+    public Question(int questionTextId, boolean answerTrue) {
+        this.questionTextId = questionTextId;
+        this.answerTrue = answerTrue;
+    }
+
+    public int getQuestionTextId() {
+        return questionTextId;
+    }
+
+    public void setQuestionTextId(int questionTextId) {
+        this.questionTextId = questionTextId;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
