fce6976db8fd0d506c2694d9895522f10ec8f74b
ekaterinasob <69851584+ekaterinasob@users.noreply.github.com>
2020-09-17 12:44:55 +0300

    Update AnswerActivity.java

--- task_8/AnswerActivity.java
+++ task_8/AnswerActivity.java
@@ -1,1 +1,29 @@
+package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity extends AppCompatActivity {
+
+    private boolean isAnswerTrue;
+    private TextView answerTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+
+        isAnswerTrue = getIntent().getBooleanExtra("answer",false);
+        answerTextView = findViewById(R.id.answerTextView);
+
+        // answerTextView.setText("Ответ на вопрос: "+(isAnswerTrue?"Да":"Нет"));
+
+        if (isAnswerTrue)
+            answerTextView.setText("Ответ на вопрос Да");
+        else
+            answerTextView.setText("Ответ на вопрос Нет");
+    }
+}
